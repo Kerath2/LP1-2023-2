bfa78bbaaa2f8f70861b9a0cb1df354eac74c163
kerath <kerath2@github.com>
2023-08-16 11:24:08 -0500

    utilizando libreria manip

--- 16-08/funciones/main.cpp
+++ 16-08/funciones/main.cpp
@@ -1,13 +1,15 @@
 #include <iostream>
 #include <iomanip>
 #include "funciones.h"
+#include <iomanip>
+
 using namespace std;
 
 int main(){
   regNotas notas = {{1,2,3,4,5,6,7,8,9,10},10};
   // for (int i = 0 ; i < notas.n ; i++)
   //   cout << notas.lab[i] << endl;
-  cout << promedio(notas.lab,10);
+  cout << setw(10) << fixed << setprecision(2) << promedio(notas.lab,10);
 
 
   return 0;
